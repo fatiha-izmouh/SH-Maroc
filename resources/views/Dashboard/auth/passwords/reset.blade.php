65e927e067c6c11cda3229513f5915837b5c2b3c
fatiha-izmouh <izmouh2003@gmail.com>
2024-11-20 15:51:14 +0100

    logos

--- resources/views/Dashboard/auth/passwords/reset.blade.php
+++ resources/views/Dashboard/auth/passwords/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('Dashboard.layouts.app')
 
 @section('content')
 <div class="container">
--- resources/views/Dashboard/layouts/admin.blade.php
+++ resources/views/Dashboard/layouts/admin.blade.php
@@ -19,7 +19,7 @@
         <nav class="navbar-vertical navbar">
             <div class="nav-scroller">
                 <a class="navbar-brand">
-                    <img src="{{ asset('admin_assets/images/brand/logo/logo.svg') }}" alt="" />
+                    <img src="{{ asset('site\images\Logos\shmaroc.png') }}" style="width: 150px; height: 120px;" alt="" />
                 </a>
                 <ul class="navbar-nav flex-column" id="sideNavbar">
                 <li class="nav-item {{ request()->is('admin') ? 'active' : '' }}">
@@ -67,60 +67,31 @@
                 </ul>
             </div>
         </nav>
-        
         <div id="page-content">
             <div class="header">
                 <nav class="navbar-classic navbar navbar-expand-lg">
-                    <a id="nav-toggle" href="#"><i data-feather="menu" class="nav-icon me-2 icon-xs"></i></a>
+                    
                     <div class="ms-lg-3 d-none d-md-none d-lg-block">
                         <form class="d-flex align-items-center">
                             <input type="search" class="form-control" placeholder="Search" />
                         </form>
                     </div>
                     <ul class="navbar-nav navbar-right-wrap ms-auto d-flex nav-top-wrap">
-                        <li class="dropdown stopevent">
-                            <a class="btn btn-light btn-icon rounded-circle indicator indicator-primary text-muted" href="#" role="button" id="dropdownNotification" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="icon-xs" data-feather="bell"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end" aria-labelledby="dropdownNotification">
-                                <div>
-                                    <div class="border-bottom px-3 pt-2 pb-3 d-flex justify-content-between align-items-center">
-                                        <p class="mb-0 text-dark fw-medium fs-4">Notifications</p>
-                                        <a href="#" class="text-muted">
-                                            <span><i class="me-1 icon-xxs" data-feather="settings"></i></span>
-                                        </a>
-                                    </div>
-                                    <ul class="list-group list-group-flush notification-list-scroll">
-                                        <!-- Example notification -->
-                                        <li class="list-group-item bg-light">
-                                            <a href="#" class="text-muted">
-                                                <h5 class="mb-1">Sample Notification</h5>
-                                                <p class="mb-0">This is a sample notification message.</p>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                    <div class="border-top px-3 py-2 text-center">
-                                        <a href="#" class="text-inherit fw-semi-bold">View all Notifications</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
+                        
                         <li class="dropdown ms-2">
                             <a class="rounded-circle" href="#" role="button" id="dropdownUser" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <div class="avatar avatar-md avatar-indicators avatar-online">
-                                <img src="https://www.gravatar.com/avatar/{{ md5(strtolower(trim(Auth::user()->email))) }}" alt="avatar" class="rounded-circle" />
+                                <img src="https://img.icons8.com/?size=100&id=7Ffvtg1xmgaV&format=png&color=000000{{ md5(strtolower(trim(Auth::user()->email))) }}" alt="avatar" class="rounded-circle" />
                                 </div>
                             </a>
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser">
                                 <div class="px-4 pb-0 pt-2">
                                     <div class="lh-1">
                                         <h5 class="mb-1">{{ Auth::user()->name }}</h5>
-                                        <a href="#" class="text-inherit fs-6">Role: {{ ucfirst(Auth::user()->role) }}</a>
                                     </div>
                                     <div class="dropdown-divider mt-3 mb-2"></div>
                                 </div>
                                 <ul class="list-unstyled">
-                                    <li><a class="dropdown-item" href="#">Edit Profile</a></li>
                                     <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
                                     </li>
